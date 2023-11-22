f1a6ec47671a0b3d97d14f9a3106915bec7d647b
woutervantol <woutervantol@hotmail.nl>
2023-11-22 12:00:16 +0100

    improved readability, rewrote data, added labels

--- main.py
+++ main.py
@@ -4,141 +4,148 @@ import astropy.units as u
 import scipy as sp
 from scipy import signal
 
+save_files = False
+nrchan = 1024
+measure_time = 5
 
-delta_freq = 2.5 * u.MHz
-sample_interval = (1 / (2 * delta_freq)).to(u.s)
+sample_freq = 2.5*1e6
 
 filename1 = "/disks/strw2/RadioAstronomy2023/Interferometer_Dataant1_1420.0mhz__27.10.202315.13.26.grc"
 filename2 = "/disks/strw2/RadioAstronomy2023/Interferometer_Dataant2_1420.0mhz__27.10.202315.13.26.grc"
+filename3 = "/disks/strw2/RadioAstronomy2023/ant3_1420.0mhz__27.10.202315.13.23.grc"
+filename4 = "/disks/strw2/RadioAstronomy2023/ant4_1420.0mhz__27.10.202315.13.23.grc"
+filename5 = "/disks/strw2/RadioAstronomy2023/ant5_1420.0mhz__27.10.202315.13.24.grc"
+filename6 = "/disks/strw2/RadioAstronomy2023/ant6_1420.0mhz__27.10.202315.13.24.grc"
+filename7 = "/disks/strw2/RadioAstronomy2023/dataant7_1420.0mhz__27.10.202315.13.25.grc"
+filename8 = "/disks/strw2/RadioAstronomy2023/dataant8_1420.0mhz__27.10.202315.13.25.grc"
+
+nr_samples = int(measure_time*sample_freq - measure_time*sample_freq%nrchan)
+x = np.arange(nr_samples)/sample_freq
+
+data1 = np.fromfile(filename1, dtype=np.complex64, count=nr_samples)
+data2 = np.fromfile(filename2, dtype=np.complex64, count=nr_samples)
 
 
-wrongdata = np.fromfile(filename1, dtype=np.complex64, count=200000, offset=0)
-plt.plot(np.arange(len(wrongdata)) / (2.5*1e6), wrongdata)
+startdata = data1[:200000]
+startx = x[:200000]
+plt.plot(startx, startdata)
 plt.title("Raw data with artifact")
 plt.xlabel("seconds")
+plt.savefig("reportplots/rawdata", dpi=300)
 plt.show()
 
 #base offset is needed to filter out weird effect at the start of each signal
-base_offset = 2**17 #in nr samples
-print(f"Base offset is {base_offset / (2.5*1e6)} seconds")
+base_offset = 40000 #in nr samples
+print(f"Base offset is {base_offset/sample_freq} seconds")
 
-#Take 0.1 seconds of antenna 1 and a small part of antenna 2 and correlate antenna 2 across antenna 1
-rawdata1 = np.fromfile(filename1, dtype=np.complex64, count=200000, offset=base_offset*8)
-rawdata2 = np.fromfile(filename2, dtype=np.complex64, count=100000, offset=base_offset*8)
 
-plt.plot(np.arange(len(rawdata1)) / (2.5*1e6), rawdata1)
-plt.title(f"Raw data example shifted by {base_offset / (2.5*1e6):.3f} seconds to remove artifact")
+#Take some part of antenna 1 and a small part of antenna 2 and correlate antenna 2 across antenna 1
+delaysize1 = 200000
+delaysize2 = 100000
+delaydata1 = data1[base_offset:base_offset + delaysize1]
+delaydata2 = data2[base_offset:base_offset + delaysize2]
+delayx1 = x[base_offset:base_offset + delaysize1]
+# delayx2 = x[base_offset:base_offset + delaysize2]
+
+plt.plot(delayx1, delaydata1)
+plt.title(f"Raw data example shifted by {base_offset / sample_freq:.3f} seconds to remove artifact")
 plt.xlabel("Seconds")
+plt.savefig("reportplots/rawdatashifted", dpi=300)
 plt.show()
 
 
-corr = signal.correlate(rawdata1, rawdata2, mode="valid") #mode valid means no zero padding at the beginning and end
+print(f"Find delay using {delaysize1/sample_freq} of antenna 1 and {delaysize2/sample_freq} of antenna 2")
 
-plt.plot(signal.correlation_lags(len(rawdata1), len(rawdata2), mode="valid") / (2.5*1e6), np.real(corr))
+corr = signal.correlate(delaydata1, delaydata2, mode="valid") #mode valid means no zero padding at the beginning and end
+corrx = x[base_offset:base_offset + delaysize1 - delaysize2 + 1]
+
+plt.plot(corrx, np.real(corr))
 plt.title("Real part of correlation")
 plt.xlabel("Seconds")
+plt.savefig("reportplots/realcorr", dpi=300)
 plt.show()
 
-plt.plot(signal.correlation_lags(len(rawdata1), len(rawdata2), mode="valid") / (2.5*1e6), np.imag(corr))
+plt.plot(corrx, np.imag(corr))
 plt.title("Imaginary part of correlation")
 plt.xlabel("Seconds")
+plt.savefig("reportplots/imagcorr", dpi=300)
 plt.show()
 
-plt.plot(signal.correlation_lags(len(rawdata1), len(rawdata2), mode="valid") / (2.5*1e6), np.real(corr)**2 + np.imag(corr)**2)
+plt.plot(corrx, np.real(corr)**2 + np.imag(corr)**2)
 plt.title(f"Real(corr)$^2$ + Imag(corr)$^2$")
 plt.xlabel("Seconds")
+plt.savefig("reportplots/magcorr", dpi=300)
 plt.show()
-
 
 delay_samples = np.argmax(np.real(corr)**2 + np.imag(corr)**2)
-print(f"Delay found (after base delay): {delay_samples} samples or {delay_samples /  (2.5*1e6)} seconds")
+print(f"Delay found (after base delay): {delay_samples} samples or {delay_samples/sample_freq} seconds")
 
-
-nrchan = 2**15#int(2**16)
-t_in_chan = nrchan / (2.5*1e6)
-print(f"FFT window: {nrchan} samples or {t_in_chan} seconds")
-
-#make the total number of samples a multiple of the size of the FFT window
-measure_time = 30 #seconds
-nr_samples = int(measure_time*(2.5*1e6) - measure_time*(2.5*1e6)%nrchan)
-
-data1_5sec = np.fromfile(filename1, dtype=np.complex64, count=nr_samples, offset=base_offset*8 + delay_samples*8)
-data2_5sec = np.fromfile(filename1, dtype=np.complex64, count=nr_samples, offset=base_offset*8)
-
-data1_5sec = data1_5sec.reshape(int(nr_samples/nrchan), nrchan)
-data2_5sec = data2_5sec.reshape(int(nr_samples/nrchan), nrchan)
-
-FFT1 = np.fft.fft(data1_5sec, axis=1)
-FFT2 = np.fft.fft(data2_5sec, axis=1)
-
-plt.plot(range(len(FFT1[0])), np.real(FFT1[0]))
-plt.title("Real part of FFT not FFTshifted")
+delaydata1 = data1[base_offset + delay_samples:base_offset + delaysize1 + delay_samples]
+delaydata2 = data2[base_offset:base_offset + delaysize2]
+corr = signal.correlate(delaydata1, delaydata2, mode="valid")
+plt.plot(corrx, np.real(corr)**2 + np.imag(corr)**2)
+plt.title(f"Real(corr)$^2$ + Imag(corr)$^2$, shifted")
+plt.xlabel("Seconds")
+plt.savefig("reportplots/magcorrshifted", dpi=300)
 plt.show()
+
+
+print(f"FFT window: {nrchan} samples or {nrchan/sample_freq} seconds")
+print("measure samples: ", nr_samples, "total of ", nr_samples/(2.5*1e6), "seconds")
+
+nrwindows = int((nr_samples-base_offset-delay_samples) / nrchan)
+
+data1 = data1[base_offset+delay_samples:base_offset+delay_samples+nrwindows*nrchan].reshape(nrwindows, nrchan)
+data2 = data2[base_offset:base_offset+nrwindows*nrchan].reshape(nrwindows, nrchan)
+x = x[base_offset:base_offset+nrchan]
+
+FFT1 = np.fft.fft(data1, axis=1)
+FFT2 = np.fft.fft(data2, axis=1)
+
+freqs = np.fft.fftfreq(len(x), d=1e6/sample_freq)
+
+plt.plot(freqs, np.real(FFT1[0]))
+plt.title("Real part of FFT not FFTshifted")
+plt.xlabel("Frequency (MHz)")
+plt.savefig("reportplots/realFFTnonshifted", dpi=300)
+plt.show()
+
 
 FFT1 = np.fft.fftshift(FFT1)
 FFT2 = np.fft.fftshift(FFT2)
-
-freqs = np.fft.fftshift(np.fft.fftfreq(len(FFT1[0])))
+freqs = np.fft.fftshift(freqs)
 
 
 plt.plot(freqs, np.real(FFT1[0]))
 plt.title("Real part of FFT, shifted")
-plt.show()
-
-
-# plt.plot(freqs, np.real(FFT1[0]))
-# plt.title("Real part of FFT of single window of data1 without clock peak")
-# plt.show()
-# plt.plot(freqs, np.imag(FFT1[0]))
-# plt.title("Imaginary part of FFT of single window of data1 without clock peak")
-# plt.show()
-# plt.plot(freqs, np.real(FFT2[0]))
-# plt.title("Real part of FFT of single window of data2 after removing clock peak")
-# plt.show()
-# plt.plot(freqs, np.imag(FFT2[0]))
-# plt.title("Imaginary part of FFT of single window of data2 without clock peak")
-# plt.show()
-
-
-
-autocorrelation = np.mean(FFT1 * np.conj(FFT1), axis=0)
-
-# interpolate the clock peak
-iprange = 20
-left_index = np.argmax(autocorrelation)-iprange
-right_index = np.argmax(autocorrelation)+iprange
-autocorrelation[left_index:right_index] = np.linspace(autocorrelation[left_index], autocorrelation[right_index], 2*iprange)
-
-plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(autocorrelation)), np.real(autocorrelation))
-plt.title("Autocorrelation test real (should be like assignment 1?)")
-plt.show()
-plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(autocorrelation)), np.imag(autocorrelation))
-plt.title("Autocorrelation test imag (should be like assignment 1?)")
+plt.xlabel("Frequency (MHz)")
+plt.savefig("reportplots/realFFT", dpi=300)
 plt.show()
 
 crosscorr = np.mean(FFT1 * np.conj(FFT2), axis=0)
-# plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(crosscorr)), np.real(crosscorr))
-# plt.title(f"Real part of cross correlation of FFTs averaged over {measure_time} seconds")
-# plt.show()
-# plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(crosscorr)), np.imag(crosscorr))
-# plt.title(f"Imaginary part of cross correlation of FFTs averaged over {measure_time} seconds")
-# plt.show()
-plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(crosscorr)), np.abs(crosscorr))
-plt.title(f"Magnitude of cross correlation of FFTs averaged over {measure_time} seconds")
+
+plt.plot(freqs, np.absolute(crosscorr))
+plt.title(f"Magnitude of cross correlation of FFTs averaged over {measure_time} seconds, including clock peak")
+plt.savefig("reportplots/magcrosscorrwithpeak", dpi=300)
 plt.show()
 
+interpolationrange = 1
+peak = np.argmax(np.abs(crosscorr))
+left_index = peak - interpolationrange
+right_index = peak + interpolationrange
+crosscorr[left_index:right_index] = np.linspace(crosscorr[left_index], crosscorr[right_index], 2*interpolationrange)
 
-iprange = 30
-left_index = np.argmax(np.abs(crosscorr))-iprange
-right_index = np.argmax(np.abs(crosscorr))+iprange
-crosscorr[left_index:right_index] = np.linspace(crosscorr[left_index], crosscorr[right_index], 2*iprange)
-
-plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(crosscorr)), np.real(crosscorr))
+plt.plot(freqs, np.absolute(np.real(crosscorr)))
 plt.title(f"Real part of cross correlation of FFTs averaged over {measure_time} seconds")
+plt.savefig("reportplots/realcrosscorr", dpi=300)
 plt.show()
-plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(crosscorr)), np.imag(crosscorr))
+
+plt.plot(freqs, np.angle(crosscorr))
 plt.title(f"Imaginary part of cross correlation of FFTs averaged over {measure_time} seconds")
+plt.savefig("reportplots/imagcrosscorr", dpi=300)
+avg_range = int(nrchan/4)
+avg_angle = np.average(np.angle(crosscorr)[avg_range:-avg_range])
+plt.hlines(avg_angle, freqs[avg_range], freqs[-avg_range], colors="red")
 plt.show()
-plt.plot(np.linspace(1420 - 1.25, 1420 + 1.25, len(crosscorr)), np.abs(crosscorr))
-plt.title(f"Magnitude of cross correlation of FFTs averaged over {measure_time} seconds")
-plt.show()
+
+print("line at: ", avg_angle)
